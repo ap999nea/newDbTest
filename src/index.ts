7a0460438b750573eb9a5c7229b430987c460da7
Giulia Della Vecchia <giuliadellavecchia96@gmail.com>
2022-05-24 17:08:21 +0200

    feat: update db

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import app from "./server";
+
+const PORT = process.env.port;
+
+const connection = app.listen(PORT, () => {
+  console.log("server is up on http://localhost:4000");
+});
+
+/*process.on("SIGINT", () => {
+  connection.close(() => {
+    console.log("Server down");
+  });
+});*/
